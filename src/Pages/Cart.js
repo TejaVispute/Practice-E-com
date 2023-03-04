ab26523e2bf31d10cec89274d8cee6f304c94e1f
Tejas <tejasvispute25@gmail.com>
2023-03-04 18:18:15 +0530

    Add to cart and remove from cart functonality added

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -3,8 +3,42 @@ import { useBookContext } from '../Context/BooksContext'
 
 
 function Cart() {
-    let { cart } = useBookContext();
-    console.log(cart);
+    let { cart, setCart } = useBookContext();
+
+
+    const HandleAdd = (product) => {
+        const ProductExists = cart.find((item) => item._id === product._id);
+
+        if (ProductExists) {
+            setCart(
+                cart.map((item) =>
+                    item._id === product._id
+                        ? { ...ProductExists, quantity: ProductExists.quantity + 1 }
+                        : item
+                )
+            )
+        } else {
+            setCart([...cart, { ...product, quantity: 1 }])
+        }
+
+    }
+
+
+    const HandleRemoveProduct = (product) => {
+        const ProductExists = cart.find((item) => item._id === product._id);
+        if (ProductExists.quantity === 1) {
+            setCart(cart.filter((item) => item._id !== product._id))
+        } else {
+            setCart(
+                cart.map((item) =>
+                    item._id === product._id
+                        ? { ...ProductExists, quantity: ProductExists.quantity - 1 }
+                        : item
+                )
+            );
+        }
+    };
+    
     return (
         <>
             <div>{
@@ -14,11 +48,11 @@ function Cart() {
                 {
                     cart.map((product) => (
                         <>
-                            <h1>{product.name}</h1>
-                            <h1>Product Quantity:-{product.quantity}</h1>
+                            <h5>{product.name}</h5>
+                            <h5>Product Quantity:-{product.quantity}</h5>
                             <img width={200} src={product.image} alt="" />
-                            <button className='btn btn-outline-dark me-2'>+</button>
-                            <button className='btn btn-outline-dark'>-</button>
+                            <button onClick={() => HandleAdd(product)} className='btn btn-outline-dark me-2'>+</button>
+                            <button onClick={() => HandleRemoveProduct(product)} className='btn btn-outline-dark'>-</button>
                         </>
                     ))
                 }
